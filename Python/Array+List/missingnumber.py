5190993f3166e8bdad685741d62a739483b4bffa
jp1593 <juanpablo08082002@hotmail.com>
2026-04-30 12:45:33 -0600

    feat: add instruction of exercise missing number

--- Python/Array+List/missingnumber.py
+++ Python/Array+List/missingnumber.py
@@ -1,3 +1,8 @@
+"""
+Write a function to find the missing number in a given integer array of 1 to 100. The function takes to parameter the array and the number of 
+elements that needs to be in array. For example if we want to find missing number from 1 to 6 the second parameter will be 6. 
+"""
+
 def missing_number(arr, n):
     expected = n * (n + 1) // 2
     actual = sum(arr)
