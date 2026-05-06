47eccf99c5e761678badd4132c26f0792537dfc3
jp1593 <juanpablo08082002@hotmail.com>
2026-05-06 16:51:33 -0600

    feat: traversing and linear search for a dictionary

--- Python/Dictionaries/dictionary.py
+++ Python/Dictionaries/dictionary.py
@@ -1,4 +1,4 @@
-# Dictionary creation
+""" Dictionary creation """
 my_dictionary = dict()
 print(my_dictionary)
 
@@ -17,3 +17,28 @@ print(eng_spanish2)
 eng_spanish_list =  [("one","uno"), ("two","dos"), ("three","tres"), ("four","cuatro")] 
 eng_spanish3 = dict(eng_spanish_list)
 print(eng_spanish3)
+
+""" Dictionary update / addition of an element """
+add_dict = {'name': 'Jhonson', 'age': 30}
+# Change of value related to a key
+add_dict["age"] = 22
+# Addition of a value and its key
+add_dict['address'] = 'Germany'
+print(add_dict)
+
+""" Traversing trough a dictionary """
+def traversingDict(dictionary): 
+    for key in dictionary: 
+        print(key, dictionary[key])
+
+traversingDict(add_dict)
+
+""" Linear Search - Searching for a element in the dictionary """
+def linearSearch(dictionary, value): 
+    for key in dictionary: 
+        if dictionary[key] == value: 
+            return key, value
+    return "Value don't exist"
+
+print(linearSearch(add_dict, 22))
+
