4c403347985c77aa0e67c9f20e9af89c6b56c9e6
jp1593 <juanpablo08082002@hotmail.com>
2026-05-07 16:32:53 -0600

    feat: count word frequency exercise

---
+++ Python/Dictionaries/dictionaries_exercises.py/wordFrequency.py
@@ -0,0 +1,22 @@
+"""
+Count Word Frequency
+
+Define a function to count the frequency of words in a given list of words.
+
+Example:
+
+    words = ['apple', 'orange', 'banana', 'apple', 'orange', 'apple'] 
+    count_word_frequency(words) 
+
+Output:
+
+     {'apple': 3, 'orange': 2, 'banana': 1}
+"""
+
+words = ['apple', 'orange', 'banana', 'apple', 'orange', 'apple'] 
+
+def count_word_frequency(words):
+    wordCount = {item:words.count(item) for item in words}
+    return wordCount
+
+print(count_word_frequency(words))
