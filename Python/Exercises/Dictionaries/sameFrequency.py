a7de4634697b1b2568df60a41d490773f7dfd803
jp1593 <juanpablo08082002@hotmail.com>
2026-05-08 12:21:40 -0600

    feat: check if two lists have the same frequency of elements

---
+++ Python/Exercises/Dictionaries/sameFrequency.py
@@ -0,0 +1,26 @@
+"""
+Same Frequency
+
+Define a function which takes two lists as parameters and check if two given lists have the same frequency of elements.
+
+Example:
+
+    list1 = [1, 2, 3, 2, 1]
+    list2 = [3, 1, 2, 1, 3]
+    check_same_frequency(list1, list2)
+
+Output:
+
+False 
+"""
+
+from collections import Counter
+
+list1 = [1, 2, 3, 2, 1]
+list2 = [3, 1, 2, 1, 3]
+
+
+def check_same_frequency(list1, list2):
+   return Counter(list1) == Counter(list2)
+
+print(check_same_frequency(list1, list2))
