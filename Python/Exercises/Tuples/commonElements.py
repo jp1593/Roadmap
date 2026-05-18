85e7eddd5361ed63bf202bcfc2f0887584acac18
jp1593 <juanpablo08082002@hotmail.com>
2026-05-18 15:04:03 -0600

    feat: add common elements between two tuples

---
+++ Python/Exercises/Tuples/commonElements.py
@@ -0,0 +1,24 @@
+"""
+Common Elements
+
+Write a function that takes two tuples and returns a tuple containing the common elements of the input tuples.
+
+Example
+
+    tuple1 = (1, 2, 3, 4, 5)
+    tuple2 = (4, 5, 6, 7, 8)
+    output_tuple = common_elements(tuple1, tuple2)
+    print(output_tuple)  # Expected output: (4, 5)
+
+"""
+
+
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = (4, 5, 6, 7, 8)
+
+
+def common_elements(tuple1, tuple2):
+    return tuple(i for i in tuple1 if i in tuple2)
+
+output_tuple = common_elements(tuple1, tuple2)
+print(output_tuple) 
