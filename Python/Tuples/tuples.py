1ed04ea8f9d37e95a5e962b363f0dc95d1c61eaa
jp1593 <juanpablo08082002@hotmail.com>
2026-05-18 10:52:38 -0600

    feat: accessing tuple values

--- Python/Tuples/tuples.py
+++ Python/Tuples/tuples.py
@@ -1,3 +1,4 @@
+"""Creation of tuples"""
 # Tuple Creation
 newTuple = ('a', 'b', 'c', 'd', 'e')
 print(newTuple)
@@ -5,3 +6,17 @@ print(newTuple)
 # Built-in tuple function
 anotherTuple = tuple('abcde')
 print(anotherTuple)
+
+"""Accessing tuple values"""
+# Acessing b
+print(newTuple[1])
+
+# Acessing e
+print(newTuple[-1])
+
+# Slicing
+print(newTuple[1:3])
+
+# Linear search
+for i in newTuple: 
+    print(i)
